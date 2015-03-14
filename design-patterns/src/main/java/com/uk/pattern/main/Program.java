a39f80d5a3e8d94d3e96aec26c5630dc0924dd7d
PAULO <PAULO@192.168.1.4>
2015-03-14 02:45:59 +0000

    Design pattern Reactor

--- design-patterns/src/main/java/com/uk/pattern/main/Program.java
+++ design-patterns/src/main/java/com/uk/pattern/main/Program.java
@@ -1,13 +1,25 @@
 package com.uk.pattern.main;
 
+import java.io.IOException;
+
 import com.uk.pattern.eventdriven.impl.Event;
 import com.uk.pattern.eventdriven.impl.EventDispatcher;
 import com.uk.pattern.eventdriven.impl.Handler;
+import com.uk.pattern.reactor.Reactor;
 
 public class Program {
 	public static void main(String[] args) {
 		EventDispatcher dispatcher = new EventDispatcher();
 		dispatcher.registerChannel(Event.class, new Handler());
 		dispatcher.dispatch(new Event());
+		
+		
+		Reactor reactor;
+		try {
+			reactor = new Reactor(9900, false);
+			new Thread(reactor).start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
